c219c6ea9db85fd87d4635a2aec548d06f0e3312
rodrigoapolo <rodrigoapolodev@gmail.com>
2023-01-23 15:51:11 -0300

    Operação de instanciação da base de dados

---
+++ src/main/java/com/rodrigoapolo/workshopmongo/config/Instantiation.java
@@ -0,0 +1,26 @@
+package com.rodrigoapolo.workshopmongo.config;
+
+import com.rodrigoapolo.workshopmongo.domain.User;
+import com.rodrigoapolo.workshopmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository repository;
+    @Override
+    public void run(String... args) throws Exception {
+        repository.deleteAll();
+
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+        repository.saveAll(Arrays.asList(maria, alex, bob));
+    }
+}
