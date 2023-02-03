918f95548782ae663f86d466e8fbb0f52e6873c8
rodrigoapolo <rodrigoapolodev@gmail.com>
2023-02-03 15:42:49 -0300

    Inserção de usuário com POST

--- src/main/java/com/rodrigoapolo/workshopmongo/services/UserService.java
+++ src/main/java/com/rodrigoapolo/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.rodrigoapolo.workshopmongo.services;
 
 import com.rodrigoapolo.workshopmongo.domain.User;
+import com.rodrigoapolo.workshopmongo.dto.UserDTO;
 import com.rodrigoapolo.workshopmongo.repository.UserRepository;
 import com.rodrigoapolo.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,5 +22,13 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj){
+        return repository.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto){
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
