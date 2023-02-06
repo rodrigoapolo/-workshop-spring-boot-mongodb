4140c8cd76d88517e5615258818a806e0268c297
rodrigoapolo <rodrigoapolodev@gmail.com>
2023-02-06 16:41:58 -0300

    consulta simples com @Query

--- src/main/java/com/rodrigoapolo/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/rodrigoapolo/workshopmongo/repository/PostRepository.java
@@ -2,10 +2,13 @@ package com.rodrigoapolo.workshopmongo.repository;
 
 import com.rodrigoapolo.workshopmongo.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
 
 public interface PostRepository extends MongoRepository<Post, String> {
 
+    @Query("{ 'title': { '$regex': ?0, '$options': 'i'} }")
+    List<Post> searchTitle(String text);
     List<Post> findByTitleContainingIgnoreCase(String id);
 }
